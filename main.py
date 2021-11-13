39b0e951634fe44df674374b88ed0d479bc781f0
Kmerck88 <kevin.merck88@gmail.com>
2021-11-13 16:03:56 -0500

    initial Commit

---
+++ main.py
@@ -0,0 +1,10 @@
+# Welcome Messgae 
+
+Welcome_message= "Welcome to vacation planner!"
+
+print(Welcome_message)
+
+# Prompting users for their names 
+user_name = input("Please enter your name: ")
+
+print(Welcome_message, user_name)

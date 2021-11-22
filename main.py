c2cfcbfbefb886b532d2367bb6eb976d9e540c5f
Kevin Merck <kevin.merck88@gmail.com>
2021-11-22 08:34:07 -0500

    Week 2: Add Week Two Requirements
    Refactor code and added week Two Requirements

--- main.py
+++ main.py
@@ -9,19 +9,20 @@ user_name = input("Please enter your name: ")
 
 
 # Prompting users to choice a destination 
-answer = input("Do you want to Travel: Jamica) Jamica. Mexico)Mexico.?: ")
+answer = input("Do you want to Travel: Jamica) Please type Jamica : ")
 
 
 # Prompting users to choice a destination 
-vaca_days = (int(input(f"How many days they will spend on the trip "))
-# Prompting users to choice a destination 
+vaca_days = (int(input(f"How many days they will spend on the trip ")))
+# Prompting users for spendong money 
+spending_money = int(input("how much “spending money” would like to bring?  "))
+vaca_math = spending_money / vaca_days
 
 
 
 if answer == "Jamica":
-    print(f"{Welcome_message}{user_name}Thanks for choicing Mexico.")
-elif answer == "Mexico":
-    print(f"{Welcome_message}{user_name}Thanks for choicing Jamica")
+    print(f"{Welcome_message}{user_name}Thanks for choicing Jamica You are planning on spending {vaca_days} in {answer} and plan to bring ${spending_money}. You can spend ${vaca_math}0 per day  ")
+# elif answer == "Mexico":
+#     print(f"{Welcome_message}{user_name}Thanks for choicing Jamica")
 elif answer == "Q":
-    exit
-
+    exit 

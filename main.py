001826e5bb4fa724436b25caaeb6c38ff249fb0f
Kevin Merck <kevin.merck88@gmail.com>
2021-12-05 22:39:24 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,30 +1,43 @@
-Welcome_message = "Welcome to vacation planner! "
+print("Welcome to Vacation Budget App")
 
-# peso = 21.736730
-# jmd = 155.488
+name = input("What is your name? ")
 
-# Prompting users for their names
-user_name = input("Please enter your name: ")
+destination = input(f"Hello, {name} Please chose your destination? Choose [1]Mexico [2]Jamaica: ")
 
-# Prompting users to choice a destination
-answer = input("Please choice a place to travel [1]Mexico [2]Jamaica: ")
+USD = float(input("How much money in USD do you plan to spend on the trip? "))
 
-# Prompting users to choice a destination
-vaca_days = input("How many days will you spend on your trip ")
+destination_days = int(input("How many are days planning on staying? "))
 
-spending_money = int(input("How much spending money would you like to bring? "))
+if destination == "Mexico" or destination == "2":
 
-# from_currency = input("From Currency: ").upper()
-# to_currency = input("To Currency: ").upper()
-# print(from_currency, " To ", to_currency)
+    total_hours = destination_days * 24
+    total_minutes = destination_days * 24 * 60
 
-# from_currency = (40 * 21.7537, "")
-currency = "${:,.2f}".format(spending_money)
+    print("Great, Mexico sounds like an amazing trip!")
 
-if answer == "1" or "Mexico":
-    print(f"{Welcome_message}{user_name}Thanks for choosing Mexico.You are planning on spending {vaca_days} days in {answer} and plan to bring ${spending_money}. You can spend {spending_money * round(221.736730, 2)} MDX per day ")
-elif answer == "2" or "Jamaica":
-    print(f"{Welcome_message}{user_name}Thanks for choosing Jamaica! You are planning on spending {vaca_days} days in {answer} and plan to bring ${spending_money}. You can spend {spending_money*round(155.488, 2)} Jamaica Dollar per day ")
-else:
-    print("Please try again")
+    print(f"You will have {total_hours} hours, {total_minutes} minutes on your trip. ")
 
+    total_cost = USD * destination_days
+    cost_mxn = total_cost / 19.2
+    cost_usd = cost_mxn * 19.2
+    print(
+        f"The total cost of the trip in MXN is {cost_mxn:.2f} and the total cost of the trip in USD is ${cost_usd:.2f} ")
+    # Print a line that will just be used for formatting purposes that looks like “**********”.
+    print('**********')
+    print('It is a great country and you will have a lot of fun!')
+    print("Thanks for using the Vacation Budget App. Enjoy your vacation! ")
+
+if destination == "Jamaica" or destination == "2":
+    total_hours = destination_days * 24
+    total_minutes = destination_days * 24 * 60
+    print('Great, Jamaica sounds like an amazing trip!')
+    print(f"You will have {total_hours} hours, {total_minutes} minutes on your trip. ")
+
+    total_cost = USD * destination_days
+    cost_jdm = total_cost / 0.0064
+    cost_usd = cost_jdm * 0.0064
+    print(
+        f"The total cost of the trip in Jamaica Dollar is {cost_jdm:.2f} and the total cost of the trip in USD is {cost_usd:.2f} ")
+    print('**********')
+    print("See you in Jamaica! ")
+    print("Thanks for using the Vacation Budget App. Enjoy your vacation! ")

e380e4b25231e4532411f2a9409b740a000ae50c
Kevin Merck <kevin.merck88@gmail.com>
2021-12-06 10:17:27 -0500

    Update main.py

--- main.py
+++ main.py
@@ -2,24 +2,36 @@ print("Welcome to Vacation Budget App")
 
 name = input("What is your name? ")
 
-destination = input(f"Hello, {name} Please chose your destination? Choose [1]Mexico [2]Jamaica: ")
+user_input = input(f"Hello, {name} Please chose your destination? Choose [1]Mexico [2]Jamaica: ")
 
 USD = float(input("How much money in USD do you plan to spend on the trip? "))
 
 destination_days = int(input("How many are days planning on staying? "))
 
-if destination == "Mexico" or destination == "2":
+
+# password_attemps = 0 
+# password = "123!!"
+
+
+# while user_input < 2: 
+#  user_creds = input("Please enter your user information " )
+#  if user_creds == password: 
+
+#     print("You are successfully logged in!! ")
+
+
+if user_input == "Mexico" or user_input  == "2":
 
     total_hours = destination_days * 24
-    total_minutes = destination_days * 24 * 60
-
-    print("Great, Mexico sounds like an amazing trip!")
-
-    print(f"You will have {total_hours} hours, {total_minutes} minutes on your trip. ")
 
     total_cost = USD * destination_days
     cost_mxn = total_cost / 19.2
     cost_usd = cost_mxn * 19.2
+    total_minutes = destination_days * 24 * 60
+    print("Great, Mexico sounds like an amazing trip!")
+    print(f"You will have {total_hours} hours, {total_minutes} minutes on your trip. ")
+
+
     print(
         f"The total cost of the trip in MXN is {cost_mxn:.2f} and the total cost of the trip in USD is ${cost_usd:.2f} ")
     # Print a line that will just be used for formatting purposes that looks like “**********”.
@@ -27,15 +39,15 @@ if destination == "Mexico" or destination == "2":
     print('It is a great country and you will have a lot of fun!')
     print("Thanks for using the Vacation Budget App. Enjoy your vacation! ")
 
-if destination == "Jamaica" or destination == "2":
+if user_input  == "Jamaica" or user_input  == "2":
     total_hours = destination_days * 24
     total_minutes = destination_days * 24 * 60
-    print('Great, Jamaica sounds like an amazing trip!')
-    print(f"You will have {total_hours} hours, {total_minutes} minutes on your trip. ")
-
     total_cost = USD * destination_days
     cost_jdm = total_cost / 0.0064
     cost_usd = cost_jdm * 0.0064
+    print('Great, Jamaica sounds like an amazing trip!')
+    print(f"You will have {total_hours} hours, {total_minutes} minutes on your trip. ")
+
     print(
         f"The total cost of the trip in Jamaica Dollar is {cost_jdm:.2f} and the total cost of the trip in USD is {cost_usd:.2f} ")
     print('**********')
